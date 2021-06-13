b671445f21b8cea06509082a8d12fbe0a0bb472b
raked <raked@users.noreply.github.com>
2021-06-12 22:34:21 -0400

    Adding Transactions to a Block

--- blockchain.py
+++ blockchain.py
@@ -20,3 +20,20 @@ class Blockchain(object):
     def last_block(self):
         # Returns the last Block in the chain
         pass
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1

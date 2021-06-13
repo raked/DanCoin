3cbe83f344eeec0cbd0018cffee60c1a919684f2
raked <raked@users.noreply.github.com>
2021-06-12 23:12:57 -0400

    Registering new Nodes

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from textwrap import dedent
+from urllib.parse import urlparse
 from time import time
 from uuid import uuid4
 
@@ -10,9 +11,20 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
     def new_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
